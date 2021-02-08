f74f85fa292a1bf8a57e265df4a2e11460967b3c
Silvio <silvio.donato@cern.ch>
2021-02-08 15:55:32 +0100

    use python only to make script, manual submittion

--- skim.sh
+++ skim.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-echo "Check kinit:", klist
-klist
-
-echo "Check xrootd", root -b -l -q  root://eoscms.cern.ch//store/group/upgrade/delphes_output/YR_Delphes/Delphes342pre14/VBFHToMuMu_M125_14TeV_powheg_pythia8_200PU/VBFHToMuMu_M125_14TeV_powheg_pythia8_1.root -e 'Delphes->Draw("","")'
-root -b -l -q  root://eoscms.cern.ch//store/group/upgrade/delphes_output/YR_Delphes/Delphes342pre14/VBFHToMuMu_M125_14TeV_powheg_pythia8_200PU/VBFHToMuMu_M125_14TeV_powheg_pythia8_1.root -e 'Delphes->Draw("","")'
-
 source /cvmfs/sft.cern.ch/lcg/views/LCG_99/x86_64-centos7-gcc8-opt/setup.sh
 
 PATH=$PATH:/cvmfs/cms.cern.ch/slc7_amd64_gcc700/external/llvm/8.0.1/bin/
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/cms.cern.ch/slc7_amd64_gcc700/external/llvm/8.0.1/lib64/:.
+
+echo "Check kinit:", klist
+klist
+
+echo "klist -f"
+klist -f
+
+echo "token"
+token
+
+echo "Check xrootd", root -b -l -q  root://eoscms.cern.ch//store/group/upgrade/delphes_output/YR_Delphes/Delphes342pre14/VBFHToMuMu_M125_14TeV_powheg_pythia8_200PU/VBFHToMuMu_M125_14TeV_powheg_pythia8_1.root -e 'Delphes->Draw("","")'
+root -b -l -q  root://eoscms.cern.ch//store/group/upgrade/delphes_output/YR_Delphes/Delphes342pre14/VBFHToMuMu_M125_14TeV_powheg_pythia8_200PU/VBFHToMuMu_M125_14TeV_powheg_pythia8_1.root -e 'Delphes->Draw("","")'
 
 echo python3.8 converter.py $1 $2 
 python3.8 converter.py $1 $2
--- vbfhmumu.py
+++ vbfhmumu.py
@@ -1,10 +1,18 @@
+import os
+prod = "PROD_1_2"
+os.mkdir(prod)
+
 import htcondor  # for submitting jobs, querying HTCondor daemons, etc.
 import classad   # for interacting with ClassAds, HTCondor's internal data format
-import os
 from samples import samples
 
 schedd = htcondor.Schedd()          # get the Python representation of the scheduler
+
 nfilesPerJob = 10
+
+os.system("cp converter.py %s"%prod)
+os.system("cp variables.py %s"%prod)
+os.system("cp skim.sh %s"%prod)
 
 for sampleName in samples:
     for datasetNumber,dataset in enumerate(samples[sampleName]):
@@ -13,13 +21,12 @@ for sampleName in samples:
         files = ["root://eoscms.cern.ch/%s"%f for f in files if len(f)>1 ]
         filesGroups = [files[i:i+nfilesPerJob] for i in range(0,len(files),nfilesPerJob)]
         for fileGroupNumber, fileGroup in enumerate(filesGroups):
-#            print(files)
             hostname_job = htcondor.Submit({
                 "executable": "skim.sh",  # the program to run on the execute node
                 "arguments": "%s_%s_%s.root %s"%(sampleName,datasetNumber,fileGroupNumber,','.join(fileGroup)),       # anything the job prints to standard output will end up in this file
-                "output": "Files/vbfhmumu.$(ClusterId).%s.%s.%s.out"%(sampleName,datasetNumber,fileGroupNumber),        # anything the job prints to standard error will end up in this file
-                "error": "Files/vbfhmumu.$(ClusterId).%s.%s.%s.err"%(sampleName,datasetNumber,fileGroupNumber),          # this file will contain a record of what happened to the job
-                "log": "Files/vbfhmumu.$(ClusterId).%s.%s.%s.log"%(sampleName,datasetNumber,fileGroupNumber),          # this file will contain a record of what happened to the job
+                "output": "%s.%s.%s.out"%(sampleName,datasetNumber,fileGroupNumber),        # anything the job prints to standard error will end up in this file
+                "error": "%s.%s.%s.err"%(sampleName,datasetNumber,fileGroupNumber),          # this file will contain a record of what happened to the job
+                "log": "%s.%s.%s.log"%(sampleName,datasetNumber,fileGroupNumber),          # this file will contain a record of what happened to the job
                 "transfer_input_files": "converter.py, variables.py, skim.sh",
                 "+JobFlavour": "espresso",          
                 "should_transfer_files": "YES",
@@ -27,11 +34,20 @@ for sampleName in samples:
             #    "request_memory": "128MB",      # how much memory we want
             #    "request_disk": "128MB",        # how much disk space we want
             })
-            print("I'm submitting .... %s %s %s"%(sampleName, dataset, fileGroupNumber))
-            print(hostname_job)
-            with schedd.transaction() as txn:   # open a transaction, represented by `txn`
-                cluster_id = hostname_job.queue(txn)     # queues one job in the current transaction; returns job's ClusterId
-            print("Submitted.")
+            subFile = open("%s/%s.%s.%s.sub"%(prod,sampleName,datasetNumber,fileGroupNumber),'w')
+            subFile.write("")
+            subFile.write(str(hostname_job))
+            subFile.write("queue")
+            subFile.write("")
+            subFile.close()
 
-print(cluster_id)
+print('''
+You can submit all files with
+
+cd %s
+kinit
+rm -rf merge.sub
+for i in $(ls | grep ".sub");do cat $i >>merge.sub; echo "" >> merge.sub;done
+condor_submit merge.sub
+'''%prod)
 
